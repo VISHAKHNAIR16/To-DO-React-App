ee9e3929a12285ab7fd7b1cf1967a017fbf44e82
VISHAKHNAIR16 <vishakh.22bce7720@vitapstudent.ac.in>
2025-05-30 12:25:26 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, { useState } from 'react'
+import './App.css';
+import GoalList from './components/GoalList/GoalList';
+import NewGoal from './components/NewGoal/NewGoal';
+
+function App() {
+
+  const [courseGoals, setCourseGoals] = useState(
+    [
+      {id: "cg1", text:"Finish The course"},
+      {id: "cg2", text:"Learn All about the course"},
+      {id: "cg3", text:"Help other student in Q/A section"}
+    ]
+  );
+
+
+  const addNewGoalHandler = newGoal => {
+    setCourseGoals(courseGoals.concat(newGoal)); 
+  };
+
+  return <div className='course-goals'> 
+  <h2>Course Goals</h2>
+  <NewGoal onAddGoal={addNewGoalHandler}/>
+  <GoalList goals={courseGoals} />
+  </div>;
+}
+
+export default App;
